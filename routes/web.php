c42fca1b445e900e1373375dd23d40bcfe4ac72d
Rushikesh Dandge <95011090+rushidandge@users.noreply.github.com>
2022-12-07 01:37:16 +0530

    gallery and facilities added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\FlareClient\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/gallery', function () {
+    return View('gallery');
+});
+
+Route::get('/facilities', function(){
+    return view('facilities');
+})
